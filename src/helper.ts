32e7a3b227eaa6619bc3c3a2f3d65325e3af9979
cheekysim <minecraftcheekysim@gmail.com>
2023-04-13 12:06:33 +0100

    Initial commit

---
+++ src/helper.ts
@@ -0,0 +1,14 @@
+function hello() {
+    console.log('Hello World');
+}
+
+function goodbye() {
+    console.log('Goodbye World');
+}
+
+export { hello, goodbye };
+
+export default {
+    hello,
+    goodbye
+};
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import helper, { hello, goodbye } from './helper.js';
+
+helper.hello();
+helper.goodbye();
+
+hello();
+goodbye();
